1092b60f794c83ba33abf49afe7cd492a963cfe9
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-10-11 16:15:46 +0000

    Settings: skelton

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createHashRouter,
-  RouterProvider,
-} from 'react-router-dom';
-
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Root from './Root';
 import Home from './Home'
+import Settings from './Settings'
 import Recipe from './Recipe'
 import './index.css'
-import Root from './Root';
 
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: 'recipes/:recipeName',
-        element: <Recipe />
-      },
-    ],
-  }
-]);
+const App: React.FC = () => {
+  const router = createHashRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          path: '',
+          element: <Home />
+        },
+        {
+          path: 'settings',
+          element: <Settings />
+        },
+        {
+          path: 'recipes/:recipeName',
+          element: <Recipe />
+        },
+      ],
+    }
+  ])
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  return <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
