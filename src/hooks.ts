1c0d9e7afaf15f8aba75be74757de181f671b4c0
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-10-12 15:31:46 +0000

    useLocalStorage

---
+++ src/hooks.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+
+export const useLocalStorage =
+  <T>(key: string, fallbackValue: T | (() => T)): [T, React.Dispatch<React.SetStateAction<T>>] => {
+    const createFallbackValue = (): T =>
+      fallbackValue instanceof Function ? fallbackValue() : fallbackValue
+
+    const [value, setValueInner] = useState<T>(() => {
+      const json = localStorage.getItem(key)
+      if (json === null) {
+        return createFallbackValue()
+      }
+
+      try {
+        return JSON.parse(json)
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          return createFallbackValue()
+        }
+        throw e
+      }
+    })
+
+    const setValue = (newValue: T | ((prevState: T) => T)) => {
+      if (newValue instanceof Function) {
+        newValue = newValue(value)
+      }
+      setValueInner(newValue)
+      localStorage.setItem(key, JSON.stringify(newValue))
+    }
+
+    useEffect(() => {
+      const listener = (ev: StorageEvent) => {
+        if (ev.key !== key) return;
+
+        const json = ev.newValue
+        if (json !== null) {
+          setValueInner(JSON.parse(json))
+        } else {
+          setValueInner(createFallbackValue())
+        }
+      }
+
+      addEventListener('storage', listener)
+      return () => {
+        removeEventListener('storage', listener)
+      }
+    }, [key])
+
+    return [value, setValue]
+  }
