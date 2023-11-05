e5f4380b099b48eec52d529e442e488ba418e31f
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-11-05 17:46:13 +0000

    Migrate to preact

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { render } from 'preact'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import routes from './routes'
 
-const App: React.FC = () => {
+const App = () => {
   const router = createHashRouter(routes)
-
-  return <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  return <RouterProvider router={router} />
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+render(<App />, document.getElementById('root')!)
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import preact from '@preact/preset-vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import preload from 'vite-plugin-preload'
 import license from 'rollup-plugin-license'
-import * as cp from 'child_process';
+import * as cp from 'child_process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   plugins: [
     vanillaExtractPlugin(),
-    react(),
+    preact(),
     preload(),
   ],
   build: {
